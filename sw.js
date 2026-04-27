3ffe104290176c205ba668e60e337cf88e373fcd
ivancww <wahchiu0903@gmail.com>
2026-04-28 01:06:27 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// 每次修改 index.html 後，請一定要來這裡更改版本號！
-const CACHE_NAME = 'aia-pwa-cache-v1.0.2'; 
+const CACHE_NAME = 'aia-pwa-cache-v1.0.3'; 
 const urlsToCache = [
   './',
   './index.html',
@@ -8,35 +7,26 @@ const urlsToCache = [
   './icon-512.png'
 ];
 
-// 安裝並快取
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// 攔截請求，優先使用快取
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
-// 啟動新版本時，清除舊版快取
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+          if (cacheWhitelist.indexOf(cacheName) === -1) return caches.delete(cacheName);
         })
       );
     })
@@ -44,9 +34,6 @@ self.addEventListener('activate', event => {
   return self.clients.claim();
 });
 
-// 接收來自網頁的「強制更新」指令
-self.addEventListener('message', function(event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') self.skipWaiting();
 });
